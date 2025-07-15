fb7ee678b9fa2f7e4569dbc5d1de34f86e504164
Mebhaskar01 <bhaskarsinghisking@gmail.com>
2025-07-15 22:22:20 +0530

    Add files via upload

---
+++ chatbot.py
@@ -0,0 +1,29 @@
+{
+  "intents": [
+    {
+      "tag": "greeting",
+      "patterns": ["hi", "hello", "hey", "good morning", "good evening"],
+      "responses": ["Hello!", "Hi there!", "Hey! How can I help you?"]
+    },
+    {
+      "tag": "goodbye",
+      "patterns": ["bye", "goodbye", "see you", "exit"],
+      "responses": ["Goodbye!", "Take care!", "See you later!"]
+    },
+    {
+      "tag": "name_query",
+      "patterns": ["what's your name", "who are you"],
+      "responses": ["I'm your assistant chatbot.", "You can call me ChatBot Pro."]
+    },
+    {
+      "tag": "feeling",
+      "patterns": ["how are you", "how are you doing"],
+      "responses": ["I'm just code, but I'm doing great!", "I'm fine. Thanks for asking!"]
+    },
+    {
+      "tag": "thanks",
+      "patterns": ["thanks", "thank you", "thx"],
+      "responses": ["You're welcome!", "Glad to help!", "Anytime!"]
+    }
+  ]
+}
